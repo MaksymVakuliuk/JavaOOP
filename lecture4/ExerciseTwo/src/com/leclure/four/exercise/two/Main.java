4d3120b6293520409d12ae9ba914dff8e9e61383
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2019-08-05 19:30:52 +0300

    Add Lecture 4 Exercise 2

---
+++ lecture4/ExerciseTwo/src/com/leclure/four/exercise/two/Main.java
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//Display all numbers from 1 to 100, which
+//divided by 3 without remainder.
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+package com.leclure.four.exercise.two;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 3; i < 100; i+=3) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
