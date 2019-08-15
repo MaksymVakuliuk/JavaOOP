e66e034d857a59e7a6d9784e5703087a92fd36e0
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2019-08-15 14:50:08 +0300

    Corrected Lecture 4 Exercise 4

--- lecture4/ExerciseFour/src/com/lecture/four/exercise/four/Main.java
+++ lecture4/ExerciseFour/src/com/lecture/four/exercise/four/Main.java
@@ -10,11 +10,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = {3, 1, 2, 3, 4, 5, 6, 7, 8,5};
+        int[] array = {3, 1, 2, 3, 4, 5, 6, 7, 8, 5};
+        int sum = 0;
+
+        System.out.println("Array: ");
+
+        System.out.print(array[0] + " ");
 
         for (int index = 1; index < array.length - 1; index++) {
             System.out.print(array[index] + " ");
+            sum += array[index];
         }
+
+        System.out.println(array[array.length-1]);
+
+        System.out.println("The sum of all its elements except the first and last one: " + sum);
 
     }
 }
